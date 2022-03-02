e65d068056146cfc495dcce6cb3fd49fd9bb29d2
euidml <elee323@my.bcit.ca>
2022-03-01 20:02:26 -0800

    make layout of login page

--- circle/src/App.js
+++ circle/src/App.js
@@ -1,38 +1,10 @@
-import logo from "./oring_logo.png";
-import "./App.css";
-import * as React from 'react';
-import ReactDOM from 'react-dom';
-// import Button from '@mui/material/Button';
-import { Grid,Paper, Avatar, TextField, Button, Typography,Link } from '@material-ui/core'
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-
-
+import React from 'react';
+import './App.css';
+import Login from './login.js';
 function App() {
-  const paperStyle={padding :20,height:'70vh',width:280, margin:"20px auto"}
-  const avatarStyle={backgroundColor:'#1bbd7e'}
-  const btnstyle={margin:'8px 0'}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Circle
-        </p>
-        {/* <Button variant="text">Text</Button> */}
-        <Button variant="contained">Contained</Button>
-        {/* <Button variant="outlined">Outlined</Button> */}
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-        
-      </header>
+      <Login />
     </div>
   );
 }
--- circle/src/login.js
+++ circle/src/login.js
@@ -3,17 +3,19 @@ import { Grid,Paper, Avatar, TextField, Button, Typography,Link } from '@materia
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import logo from './oring_logo.png'
+
 const Login=()=>{
 
     const paperStyle={padding :20,height:'70vh',width:280, margin:"20px auto"}
-    const avatarStyle={backgroundColor:'#1bbd7e'}
-    const btnstyle={margin:'8px 0'}
+    const logoStyle={width: "100%"}
+    const btnstyle={margin:'8px 0', backgroundColor: "Black"}
     return(
         <Grid>
             <Paper elevation={10} style={paperStyle}>
                 <Grid align='center'>
-                     <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
-                    <h2>Sign In</h2>
+                    <img src={logo} className="App-logo" alt="logo" style={logoStyle}/>
+                    <h2>Sign Into Circle</h2>
                 </Grid>
                 <TextField label='Username' placeholder='Enter username' fullWidth required/>
                 <TextField label='Password' placeholder='Enter password' type='password' fullWidth required/>
