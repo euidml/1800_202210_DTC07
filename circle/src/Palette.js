d374c20eb7cfd76c1bac1f31050fcb71eced495c
euidml <elee323@my.bcit.ca>
2022-03-08 22:52:34 -0800

    set theme colours

---
+++ circle/src/Palette.js
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material/styles';
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#00afef",
+      },
+      secondary: {
+        main: "#fff212",
+      },
+    },
+  });
