b50ee05e3b9da6a984ad0eb7b69e8aec016c7c29
euidml <elee323@my.bcit.ca>
2022-04-07 09:58:22 -0700

    implment favouite and creating chat room on DB

--- circle/src/Favorite.js
+++ circle/src/Favorite.js
@@ -1,38 +1,94 @@
-import React from 'react';
-import IconButton from '@mui/material/IconButton';
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import React, { useEffect, useState } from "react";
+import IconButton from "@mui/material/IconButton";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import { Link } from "react-router-dom";
-import './Favorite.css';
-
-
+import "./Favorite.css";
+import { async } from "@firebase/util";
+import {
+  collection,
+  getDoc,
+  doc,
+  query,
+  getDocs,
+  where,
+  documentId,
+  setDoc,
+  addDoc,
+  Timestamp,
+  updateDoc,
+  arrayUnion
+} from "firebase/firestore";
+import { auth, db } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 function Favorite() {
-    return (
-        <div>
-        {/* <Link to="/dashboard">
+  const [name, setName] = useState("")
+  const [user] = useAuthState(auth);
+  const [favouritePeople, setFavourtiePeople] = useState([]);
+  const [fetchStatus, setFetchStatus] = useState(true)
+  const fetchFavouriteProfiles = async () => {
+    try {
+      if (fetchStatus) {
+        const docRef = doc(db, "UserInfo", user?.uid);
+        const userData = (await getDoc(docRef)).data()
+        setName(userData.name)
+        const userFavouriteList = userData.likedPeople;
+        const userRef = collection(db, "UserInfo");
+        const q = query(userRef, where(documentId(), "in", userFavouriteList));
+        const data = await getDocs(q);
+        console.log(data.docs);
+        // setFavourtiePeople((favouritePeople) => [])
+        data.forEach((doc) => {
+          if (!(doc.data() in favouritePeople)) {
+            console.log(doc.id)
+            favouritePeople.push([doc.data(), doc.id]);
+          }
+        });
+        console.log(favouritePeople);
+        setFetchStatus(false)
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const createChatRoom = async (partnerUid, partnerName) => {
+      const docRef = await addDoc(collection(db, "chatRooms"),{
+          chatLogs:[],
+          infos: Timestamp.fromDate(new Date()),
+          users:[{name:name, uid: user.uid}, {name:partnerName, uid:partnerUid}]
+      })
+      await updateDoc(doc(db, "UserInfo", user.uid),{
+          chatRooms: arrayUnion({
+              chatRoom: docRef.id,
+              partnerUid: partnerUid
+            })
+        }
+        )
+    console.log(docRef.id, partnerUid)
+  }
+  const rendering = () => {
+    return favouritePeople.map((person) => (
+      <div className="images">
+        <img alt={person[0].name} src={person[0].profilePhoto.photo} onClick={() => createChatRoom(person[1], person[0].name)}></img>
+      </div>
+    ));
+  };
+  useEffect(() => {
+    fetchFavouriteProfiles();
+  }, []);
+
+  return (
+    <div>
+      {/* <Link to="/dashboard">
         <IconButton>
         <ArrowBackIosNewIcon className='backarrow' fontSize='large' />
         </IconButton>
         </Link> */}
-        
-        <h2 className='fav'>💛 Your favorited people</h2>
-        <div className='img_container'>
-            <div className='images'>
-                <img src='https://i2-prod.mirror.co.uk/incoming/article11167826.ece/ALTERNATES/n310p/0_Thylane-Blondeau.jpg' alt='ed'></img>
-            </div>
 
-            <div className='images'>
-                <img src='https://media1.popsugar-assets.com/files/thumbor/KOb-u-ocyOE0Mw27W17OZN1QOSQ/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2016/03/18/896/n/1922398/1385d1ed_edit_img_image_36158137_1416727436_JMomoa/i/Jason-Momoa.jpg' alt='jason'></img>
-            </div>
-
-            <div className='images'>
-                <img src='https://friendlystock.com/wp-content/uploads/2020/04/6-mexican-chicken-cartoon-clipart.jpg' alt='run'></img>
-            </div>
-
-            
-        </div>
-        </div>
-    );
+      <h2 className="fav">💛 Your favorited people</h2>
+      <div className="img_container">{rendering()}</div>
+    </div>
+  );
 }
 
 export default Favorite;
